d2240a7c7dbb0fba42d543e3a5c4c3a8a285852e
BuildTools <unconfigured@null.spigotmc.org>
2021-01-12 22:41:58 +0300

    s

---
+++ leetcode08/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestAddTwoNumbers(t *testing.T) {
+	type args struct {
+		l1 *ListNode
+		l2 *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := AddTwoNumbers(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("AddTwoNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
